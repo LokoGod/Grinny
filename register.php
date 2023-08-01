777fc9f49a61b00038ff1aee40e66dee7397522a
LokoGod <it21079672@my.sliit.lk>
2023-08-01 12:18:51 +0530

    register_design

--- register.php
+++ register.php
@@ -4,96 +4,97 @@ ini_set('display_errors', 1);
 
 // Include config file
 require_once "db_connection.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-  $username = trim($_POST["username"]);
- 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-        $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM register WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$username = trim($_POST["username"]);
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-    
-    // Check input errors before inserting in the database
-    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
-      // Prepare an insert statement
-      $sql = "INSERT INTO register (username, password) VALUES (?, ?)";
+// Validate username
+if (empty(trim($_POST["username"]))) {
+$username_err = "Please enter a username.";
+} elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
+$username_err = "Username can only contain letters, numbers, and underscores.";
+} else {
+// Prepare a select statement
+$sql = "SELECT id FROM register WHERE username = ?";
 
-      if ($stmt = mysqli_prepare($link, $sql)) {
-          // Bind variables to the prepared statement as parameters
-          mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+if ($stmt = mysqli_prepare($link, $sql)) {
+// Bind variables to the prepared statement as parameters
+mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-          // Set parameters
-          $param_username = $username;
-          $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+// Set parameters
+$param_username = trim($_POST["username"]);
 
-          // Attempt to execute the prepared statement
-          if (mysqli_stmt_execute($stmt)) {
-              // Redirect to login page
-              header("location: logIn.php");
-          } else {
-              echo "Oops! Something went wrong. Please try again later.";
-          }
+// Attempt to execute the prepared statement
+if (mysqli_stmt_execute($stmt)) {
+/* store result */
+mysqli_stmt_store_result($stmt);
 
-          // Close statement
-          mysqli_stmt_close($stmt);
-      }
-  }
-  // Close connection
-  mysqli_close($link);
+if (mysqli_stmt_num_rows($stmt) == 1) {
+$username_err = "This username is already taken.";
+} else {
+$username = trim($_POST["username"]);
+}
+} else {
+echo "Oops! Something went wrong. Please try again later.";
+}
+
+// Close statement
+mysqli_stmt_close($stmt);
+}
+}
+
+// Validate password
+if (empty(trim($_POST["password"]))) {
+$password_err = "Please enter a password.";
+} elseif (strlen(trim($_POST["password"])) < 6) {
+$password_err = "Password must have atleast 6 characters.";
+} else {
+$password = trim($_POST["password"]);
+}
+
+// Validate confirm password
+if (empty(trim($_POST["confirm_password"]))) {
+$confirm_password_err = "Please confirm password.";
+} else {
+$confirm_password = trim($_POST["confirm_password"]);
+if (empty($password_err) && ($password != $confirm_password)) {
+$confirm_password_err = "Password did not match.";
+}
+}
+
+// Check input errors before inserting in the database
+if (empty($username_err) && empty($password_err) &&
+empty($confirm_password_err)) {
+// Prepare an insert statement
+$sql = "INSERT INTO register (username, password) VALUES (?, ?)";
+
+if ($stmt = mysqli_prepare($link, $sql)) {
+// Bind variables to the prepared statement as parameters
+mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+// Set parameters
+$param_username = $username;
+$param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+// Attempt to execute the prepared statement
+if (mysqli_stmt_execute($stmt)) {
+// Redirect to login page
+header("location: logIn.php");
+} else {
+echo "Oops! Something went wrong. Please try again later.";
+}
+
+// Close statement
+mysqli_stmt_close($stmt);
+}
+}
+// Close connection
+mysqli_close($link);
 }
 
 ?>
@@ -162,72 +163,84 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       </div>
     </nav>
 
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+    <br>
+    <div class="card text-center container">
+      <div class="card-header wrapper text-center">
+        <h3><strong>Register</strong></h3>
+      </div>
+      <div class="card-body">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+          method="post" class="mx-auto" style="max-width: 400px;">
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username"
+              class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+              value="<?php echo $username; ?>">
+            <span class="invalid-feedback"><?php echo $username_err; ?></span>
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password"
+              class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+              value="<?php echo $password; ?>">
+            <span class="invalid-feedback"><?php echo $password_err; ?></span>
+          </div>
+          <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="confirm_password"
+              class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"
+              value="<?php echo $confirm_password; ?>">
+            <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+          </div>
+          <br>
+          <div class="form-group text-center">
+            <input type="submit" class="btn btn-primary" value="Submit">
+            <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+          </div>
+          <br>
+          <p>Already have an account? <a href="logIn.php">Login here</a>.</p>
         </form>
-    </div>    
-
-
-
-
-
-
-<footer>
-    <div id="footer-name">
-      <h2>Grinny</h2>
-      <p>
-        Your Safety is Our Priority: Discover the Secure World of Our LLM
-        Program!
-      </p>
-      <div id="footer-social-icon">
-        <i class="fa-brands fa-instagram"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-linkedin"></i>
-        <i class="fa-brands fa-facebook"></i>
+      </div>
+      <div class="card-footer text-body-secondary">
+        <p><strong>Please fill this form to create an account.</strong></p>
       </div>
     </div>
+    <br>
 
-    <div class="footer-link"></div>
+    <footer>
+      <div id="footer-name">
+        <h2>Grinny</h2>
+        <p>
+          Your Safety is Our Priority: Discover the Secure World of Our LLM
+          Program!
+        </p>
+        <div id="footer-social-icon">
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-facebook"></i>
+        </div>
+      </div>
 
-    <div class="footer-link">
-      <h4>About Us</h4>
-      <div>
-        <a href="aboutUs.html">Projects</a>
-        <a href="aboutUs.html">Strategies</a>
-        <a href="aboutUs.html">Press</a>
-        <a href="aboutUs.html">Mission</a>
+      <div class="footer-link"></div>
+
+      <div class="footer-link">
+        <h4>About Us</h4>
+        <div>
+          <a href="aboutUs.html">Projects</a>
+          <a href="aboutUs.html">Strategies</a>
+          <a href="aboutUs.html">Press</a>
+          <a href="aboutUs.html">Mission</a>
+        </div>
       </div>
-    </div>
-    <div class="footer-link">
-      <h4>Support</h4>
-      <div>
-        <a href="contactUs.html">Developer</a>
-        <a href="contactUs.html">Support</a>
-        <a href="contactUs.html">Customer Service</a>
-        <a href="contactUs.html">Get started</a>
-        <a href="contactUs.html">Guide</a>
+      <div class="footer-link">
+        <h4>Support</h4>
+        <div>
+          <a href="contactUs.html">Developer</a>
+          <a href="contactUs.html">Support</a>
+          <a href="contactUs.html">Customer Service</a>
+          <a href="contactUs.html">Get started</a>
+          <a href="contactUs.html">Guide</a>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
