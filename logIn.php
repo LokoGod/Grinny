f4197ff5426a34cfdd6758983f841dab998d3a96
LokoGod <it21079672@my.sliit.lk>
2023-08-01 15:14:28 +0530

    new

--- logIn.php
+++ logIn.php
@@ -1,3 +1,91 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Include config file
+require_once "db_connection.php";
+
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect to welcome page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: welcome.php");
+    exit;
+}
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+
+    // Validate credentials
+    if (empty($username)) {
+        $username_err = "Please enter your username.";
+    }
+
+    if (empty($password)) {
+        $password_err = "Please enter your password.";
+    }
+
+    // Check input errors before verifying the user
+    if (empty($username_err) && empty($password_err)) {
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM register WHERE username = ?";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    mysqli_stmt_bind_result($stmt, $user_id, $username, $hashed_password);
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
+                            // Password is correct, start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["user_id"] = $user_id;
+                            $_SESSION["username"] = $username;
+
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else {
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered is not valid.";
+                        }
+                    }
+                } else {
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -64,27 +152,48 @@
     </nav>
 
     <div class="panel">
-      <div class="state"><br><i class="fa fa-unlock-alt"></i><br><h1>Log in</h1></div>
-      <div class="form">
-        <input placeholder='Email' type="email" required>
-        <input placeholder='Password' type="password" required>
-        <button class="wide-button" type="submit">Login</button>
-      </div>
-      <div class="fack"><a href="register.php"><i class="fa fa-question-circle"></i>Or register with us</a></div>
+        <div class="state">
+            <br>
+            <i class="fa fa-unlock-alt"></i>
+            <br>
+            <h1>Log in</h1>
+        </div>
+        <div class="form">
+            <form action="logIn.php" method="post">
+                <div class="form-group">
+                    <input type="text" name="username" class="form-control" placeholder="Username" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
+                </div>
+                <div class="form-group text-center">
+                    <button class="wide-button" type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+        <div class="fack"><a href="register.php"><i class="fa fa-question-circle"></i>Or register with us</a></div>
     </div>
 
     <script>
-      $(document).ready(function() {
-  return $('.login').click(function(event) {
-    if (false) {
+        // JavaScript to handle form submission (replace the dummy check with actual form submission)
+        // For real implementation, the form should be submitted to the login_backend.php file.
+        document.querySelector('form').addEventListener('submit', function (event) {
+            event.preventDefault();
+            var username = document.querySelector('input[name="username"]').value;
+            var password = document.querySelector('input[name="password"]').value;
 
-    } else {
-      return $('.state').html('<br><i class="fa fa-ban"></i><br><h2>Error</h2>The email or password you entered is incorrect, please try again.').css({
-        color: '#eb5638'
-      });
-    }
-  });
-});
+            if (false) {
+                // If login is successful, redirect the user to the welcome page
+                window.location.href = "welcome.php";
+            } else {
+                // If login fails, show an error message
+                var stateDiv = document.querySelector('.state');
+                stateDiv.innerHTML = '<br><i class="fa fa-ban"></i><br><h2>Error</h2>The email or password you entered is incorrect, please try again.';
+                stateDiv.style.color = '#eb5638';
+            }
+        });
     </script>
 
-  </body>
+</body>
+
+</html>
